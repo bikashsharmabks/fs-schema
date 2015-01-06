f4ba83a6c2536ca41949f4cd6b33ad2722cf4fe9
Bikash Kumar Sharma <bikash1999@yahoo.com>
2015-01-06 16:49:00 +0530

    changed

--- fs-schema.js
+++ fs-schema.js
@@ -6,24 +6,33 @@
  * Module dependencies.
  * @private
  */
-
-
-
 var fs = require('fs'),
   path = require("path");
 
 var walk = function(dir, done) {
   var results = [];
-  function makeFileObject(p){
+
+  function makeFileObject(p) {
     var name = path.basename(p, path.extname(p));
     var parent = path.dirname(p);
-    return {"name": name, "url": p, "relative": p, "parent": parent};
+    return {
+      "name": name,
+      "url": p,
+      "relative": p,
+      "parent": parent
+    };
   };
 
-  function makeFolderObject(p){
+  function makeFolderObject(p) {
     var parent = path.dirname(p);
     var folders = p.split(parent);
-    return {"name": folders[1], "url": p, "children":[], "relative": p, "parent": parent};
+    return {
+      "name": folders[1],
+      "url": p,
+      "children": [],
+      "relative": p,
+      "parent": parent
+    };
   };
 
   fs.readdir(dir, function(err, list) {
@@ -57,38 +66,46 @@ var walk = function(dir, done) {
 
 
 // Traverse data and build the tree
-var buildTree = function(arr, tree) {
-    var tree = { "root": arr[0]};
+var buildTree = function(arr) {
+  var tree = {
+    "root": arr[0]
+  };
 
-    for (var i = 0; i < arr.length; i++) {
-        var elem = arr[i];
-        var rootId = elem.parent;
-        var parent = getParent(tree.root, rootId);
-        if (parent) {
-            parent.children.push(elem);
-        }
+  for (var i = 0; i < arr.length; i++) {
+    var elem = arr[i];
+    var rootId = elem.parent;
+    var parent = getParent(tree.root, rootId);
+    if (parent) {
+      parent.children.push(elem);
     }
-    return tree.root;
+  }
+  return tree.root;
 };
 
 // Get parent of node (recursive)
-var getParent = function (rootNode, rootId) {
-    if (rootNode.relative === rootId)
-        return rootNode;
+var getParent = function(rootNode, rootId) {
+  if (rootNode.relative === rootId)
+    return rootNode;
 
-    for (var i = 0; i < rootNode.children.length; i++) {
-        var child = rootNode.children[i];
-        if (child.relative === rootId) return child;
+  for (var i = 0; i < rootNode.children.length; i++) {
+    var child = rootNode.children[i];
+    if (child.relative === rootId) return child;
 
-        if ((child.children) && (child.children.length > 0))
-            var childResult = getParent(child, rootId);
+    if ((child.children) && (child.children.length > 0))
+      var childResult = getParent(child, rootId);
 
-        if (childResult != null) return childResult;
-    }
-    return null;
+    if (childResult != null) return childResult;
+  }
+  return null;
 };
 
-walk("./schema", function(err, results) {
-  var tree = buildTree(results);
-  console.log(JSON.stringify(tree, null, 4));
+var build = function(folder, cb) {
+  walk(folder, function(err, results) {
+    var schema = buildTree(results);
+    cb(err, schema);
+  });
+}
+
+build("./schema",function(err, schema){
+  console.log(JSON.stringify(schema, null, 4));
 });
